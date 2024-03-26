0f51ec4360acf36c87d3e8dd600f487c8e0cee61
msamgan <mohdsamgankhan@gmail.com>
2024-03-26 16:21:51 -0400

    updated the config.

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Lindsey Reid Photography'),
 
     /*
     |--------------------------------------------------------------------------
